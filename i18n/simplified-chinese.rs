24b1f461c9268a4c4f1685ced1b4fab3cf71aa35
harrison <seermole766@gmail.com>
2021-05-13 21:13:46 +0800

    Translations update for Nomad 1.50
    Some corrections

--- i18n/simplified-chinese.rs
+++ i18n/simplified-chinese.rs
@@ -105,8 +105,8 @@ background.blur "模糊"
 background.exposure "曝光"
 
 background.imageEnable "参考图像"
-background.imageOverlay "对象透明度"
-background.imageAlpha "背景透明度"
+background.imageOverlay "对象不透明度"
+background.imageAlpha "背景不透明度"
 background.imageReset "重设"
 background.imageTransform "背景变换"
 // transform
